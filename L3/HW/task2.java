b62b79942f1523fc946921cea8d704c1d95d286a
Vejber <105496392+Vejber@users.noreply.github.com>
2022-12-08 13:24:01 +0400

    list remove i

---
+++ L3/HW/task2.java
@@ -0,0 +1,30 @@
+// Пусть дан произвольный список целых чисел. Удалить из него чётные числа
+package L3.HW;
+
+import java.util.ArrayList;
+
+public class task2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        for (int i = 0; i < list.size(); i++) {
+            if (list(i) % 2 == 0) {
+                list.remove(i + 1);
+            }
+        }
+        System.out.println(list);
+    }
+
+    private static int list(int i) {
+        return 0;
+    }
+}
